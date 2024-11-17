33040ceff55e24c3c9e045cc63622a0e77eef7ab
Julien Texier <julien.texier@taitounited.fi>
2024-11-17 13:26:16 +0200

    feat: implement location tracking and custom theming in the app

--- native-app/App.tsx
+++ native-app/App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+
+import { Text } from "react-native";
+import { styled, ThemeProvider } from "./styled";
+import { useLocation } from "./utils";
 
 export default function App() {
+  const initialLocation = useLocation();
+
+  const [fontsLoaded] = useFonts({
+    Jomhuria: require("./assets/fonts/Jomhuria-Regular.ttf"),
+    Offside: require("./assets/fonts/Offside-Regular.ttf"),
+  });
+
+  if (!fontsLoaded || !initialLocation) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <Wrapper>
+        <WelcomeText>Welcome to our new game!</WelcomeText>
+        <Text>Initial location: {JSON.stringify(initialLocation)}</Text>
+        <StatusBar style="auto" />
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const Wrapper = styled("View", {
+  flex: 1,
+  backgroundColor: "#fff",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const WelcomeText = styled("Text", {
+  fontFamily: "$playful",
+  fontSize: 50,
 });
